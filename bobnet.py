3a37124ef14fe0963bd21875b0a1ad204e4b2240
Max.Keyn <max.keyn@bitctrl.de>
2022-05-17 21:10:58 +0200

    Add on_start on_end Method

--- bobnet.py
+++ bobnet.py
@@ -1,18 +1,22 @@
 from sc2.bot_ai import BotAI  # parent class we inherit from
-from sc2.data import Difficulty, Race  # difficulty for bots, race for the 1 of 3 races
-from sc2.main import run_game  # function that facilitates actually running the agents in games
-from sc2.player import Bot, Computer  #wrapper for whether or not the agent is one of your bots, or a "computer" player
-from sc2 import maps  # maps method for loading maps to play in.
 from sc2.ids.unit_typeid import UnitTypeId
 import numpy as np
 import math
-import random
 import cv2
 
 class Bob(BotAI): # inhereits from BotAI (part of BurnySC2)
+    async def on_start(self):
+        print("Game started")
+        # Do things here before the game starts
+
+    
     async def on_step(self, iteration: int): # on_step is a method that is called every step of the game.
         map = self.render_map()
         self.print_map(map)
+
+    def on_end(self, result):
+        print("Game ended.")
+        # Do things here after the game ends
 
     # Print Map with opencv
     def print_map(self, map):
